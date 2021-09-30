07c4613a30f1ff575af4e010b79c65de7693b236
faaavn <vip.vs1234@mail.ru>
2021-10-01 01:00:18 +0300

    added Task2. Excercise 4.cpp

---
+++ Task2. Exercise 4.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+using namespace std;
+
+const int sizeBoard = 3;
+char PlayBoard[sizeBoard][sizeBoard];
+
+struct Tic_tac_toe
+{
+	char players;
+	int move;
+	int score;
+
+};
+
+
+
+int main()
+{
+	cout <<' ' << PlayBoard[0][0] << ' ' << '|' << PlayBoard[0][1] << ' ' << '|' << PlayBoard[0][2] << endl; // игровое поле
+	cout <<' ' << PlayBoard[1][0] << ' ' << '|' << PlayBoard[1][1] << ' ' << '|' << PlayBoard[1][2] << endl;
+	cout <<' ' << PlayBoard[2][0] << ' ' << '|' << PlayBoard[2][1] << ' ' << '|' << PlayBoard[2][2] << endl;
+	
+ 	return 0;
+}
