9e70dc7520993bc3e90776796c2e57855ea168f2
faaavn <vip.vs1234@mail.ru>
2022-02-19 13:30:23 +0300

    Added OOP4

---
+++ OOP.4/Hands.cpp
@@ -0,0 +1,104 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+#define face_down "Face of the card is down"
+#define face_up "Face of the card is up"
+
+class Card {
+
+private:
+	enum suit { spades, hearts, diamonds, clubs };
+	enum valueC { Ace = 1, Two = 2, Three = 3, Four = 4, Five = 5, Six = 6, Seven = 7, Eight = 8, Nine = 9, Ten = 10, Jack = 11, Queen = 12, King = 13 };
+	bool positC; //position of the card
+
+public:
+
+	int flip() {
+		if (positC)
+			cout << face_down << endl;
+		return 0;
+
+		if (!positC)
+			cout << face_up << endl;
+		return 1;
+	}
+
+	int get_valueC()
+	{
+		valueC chosen_value;
+
+		switch (chosen_value)
+		{
+		case Ace:
+			return 1;
+		case Two:
+			return 2;
+		case Three:
+			return 3;
+		case Four:
+			return 4;
+		case Five:
+			return 5;
+		case Six:
+			return 6;
+		case Seven:
+			return 7;
+		case Eight:
+			return 8;
+		case Nine:
+			return 9;
+		case Ten:
+			return 10;
+		case Jack:
+			return 11;
+		case Queen:
+			return 12;
+		case King:
+			return 13;
+		}
+	}
+
+};
+
+class Hand {
+private:
+	Card* ptrC;
+
+	vector<ptrC> Han{};
+
+	int totalValue;
+
+public:
+	int add(int newCard)
+	{
+		Han.push_back(newCard);
+		
+		return newCard;
+	}
+
+	void Clear()
+	{
+		Han.clear();
+	}
+	
+	int GetValue()
+	{
+		int handValue = 0;
+
+		for (Han; ; )
+		{
+			totalValue = handValue + ptrC->get_valueC();
+		}
+		
+		cout << "The total value of cards is: " << totalValue;
+	}
+
+	
+};
+
+
+int main_cards()
+{
+
+	return 0;
+}
---
+++ OOP.4/OOP.4.cpp
@@ -0,0 +1,155 @@
+﻿#include <iostream>
+#include <string>
+#include <cassert>
+#include <vector>
+using namespace std;
+
+class ArrayInt
+{
+private:
+    int m_length;
+    int* m_data;
+public:
+    ArrayInt(): m_length(0), m_data (nullptr){}
+
+    ArrayInt(int length) : m_length(length)
+    {
+        assert(length >= 0);
+        if (length > 0)
+            m_data = new int[length];
+        else
+            m_data = nullptr;
+    }
+    ~ArrayInt() {
+        delete[] m_data;
+    }
+    void erase()
+    {
+        delete[] m_data;
+        m_length = 0;
+        m_data = nullptr;
+    }
+
+    int& operator[](int index)
+    {
+        return m_data[index];
+    }
+
+    void resize(int newLength)
+    {
+        if (newLength <= 0)
+        {
+            erase();
+            return;
+        }
+        int* newData = new int[newLength] {0};
+        int CopyElements = 0;
+        if (newLength > m_length) CopyElements = m_length;
+        else CopyElements = newLength;
+        for (size_t i = 0; i < CopyElements; i++)
+        {
+            newData[i] = m_data[i];
+        }
+        delete[]m_data;
+        m_data = newData;
+        m_length = newLength;
+    }
+    void at(int index, int data)
+    {
+        if (index >= 0 && index < m_length)
+            m_data[index] = data;
+    }
+    void push_back(int value)
+    {
+        int* newData = new int[m_length+ 1] {0};
+        int CopyElements = m_length;
+        for (size_t i = 0; i < CopyElements; i++)
+        {
+            newData[i] = m_data[i];
+        }
+        newData[m_length] = value;
+        delete[]m_data;
+        m_data = newData;
+        m_length++;
+    }
+    void pop_back(int value)                        //delete last element
+    {
+        int* newData = new int[m_length - 1]{ 0 };
+        int CopyElements = m_length -1;
+        for (size_t i = 0; i < CopyElements; i++)
+        {
+            newData[i] = m_data[i];
+        }
+        newData[m_length] = value;
+        delete[]m_data;
+        m_data = newData;
+        m_length--;
+    }
+
+    void pop_front(int value, int newLength)                      //delete first element 
+    {                                           
+        int* newData = new int[m_length - 1]{ 0 };              //tak i ne ponyal, chem otlichaetsa ot pop_back
+       
+    }
+
+    void printArray(int* arr)
+    {
+        for (int i = 0; i < m_length; i++)
+        {
+         
+                cout << arr[i]<< " ";
+            
+        }
+        cout << endl;
+    }
+
+    void insertSort(int* array)
+    {
+        int temp, pos;
+
+        for (int i = 0; i < m_length; i++)
+        {
+            temp = array[i];
+            pos = i - 1;
+            while (pos >= 0 && array[pos] > temp)
+            {
+                array[pos + 1] = array[pos];
+                pos--;
+            }
+            array[pos + 1] = temp;
+        }
+    }
+};
+
+
+int main()
+{
+    vector<int>Digit{ 1,4,5,6,8,8,9,12,14,33,45,68 };
+    int index = 0;
+
+    for (size_t i = 1; i < Digit.size(); i++)
+    {
+        for (size_t j = 0; j < Digit.size(); j++)
+        {
+            
+            Digit[j + 1];
+            
+            if (Digit[j].size() != Digit[i - 1].size())
+            {
+                index++;
+            }
+
+        }
+        
+            
+    }
+
+    cout << index << endl; //tozhe zaputal`sa s zadaniem, takay zadumka bila
+
+
+
+
+    return 0;
+}
+
+
